337e12713284405683fc678a6df0ece511effc24
Lahiru Kalhara <lahirukalharadimbulkumbura@gmail.com>
2025-02-19 21:14:45 +0530

    Hero Section Implemented

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,15 @@ import "./Hero.css";
 const Hero = () => {
   return (
     <div className="hero">
+      <div className="glowing-lines">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
       <div className="hero-content">
         <h1>Welcome to <span>Milano Cineplex</span></h1>
         <p>Experience the Future of Cinema with Ultra HD & Immersive Sound</p>
